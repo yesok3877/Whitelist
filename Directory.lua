0eb16c95572d2010f75d5b6e2c7671f334fdc532
yesok <70432888+yesok3877@users.noreply.github.com>
2021-05-27 19:57:49 -0500

    Update Directory.lua

--- Directory.lua
+++ Directory.lua
@@ -1,1 +1,12 @@
-
+return {
+    [1] = "3xf-*em\uq\pz=z36duui",
+    [2] = "`e.`.3(",
+    [3] = "}3:dgga",
+    [4] = "48a[v13v",
+    [5] = "^89yid/?o1(:i#.zf9>rc=;9o",
+    [6] = "h%41%b%<sv9.7ha",
+    [7] = "s?b|5eek3b",
+    [8] = '@m#5<"1_m]',
+    [9] = "hrxipcop+.mf",
+    [10] = "t'gqjiqn]29?h0qdsbegme"
+}
